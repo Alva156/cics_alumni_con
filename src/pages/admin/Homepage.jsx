891234b57400d5bf3175fba2d3256aea8ff1cde2
Cid Alvarico <andreicimoune.alvarico.cics@ust.edu.ph>
2024-08-18 21:49:01 +0800

    sdad

---
+++ src/pages/admin/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../../components/user/Navbar";
+import Navbar_admin from "../../components/admin/Navbar_admin";
+import Header from "../../components/Header";
+import "../../App.css";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Homepage;
