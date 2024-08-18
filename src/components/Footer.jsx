b8eb4c0277743d6e4ef3ae14446a6804e8cbdaf1
Cid Alvarico <andreicimoune.alvarico.cics@ust.edu.ph>
2024-08-19 01:56:06 +0800

    adjusted footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-fgray p-4 pb-16 pt-10 text-grayt text-xxs md:text-xs">
+    <footer className="bg-fgray p-4 pb-16 pt-10 text-grayt text-xxs md:text-sm">
       <div className="ml-6">
-        <aside className="mb-4 ml-12">
+        <aside className="mb-8 ml-2 md:ml-12">
           <p>
             2024. University of Santo Tomas. College of Information and
             Computing Sciences
           </p>
         </aside>
-        <div className="flex flex-wrap gap-x-4 ml-12">
+        <p className="ml-2 text-bgray text-xxs font-light mb-2 md:text-xs md:ml-12">
+          Developers
+        </p>
+        <div className="flex flex-wrap gap-x-4 ml-2 md:ml-12 ">
           <div className="flex flex-col mr-6">
             <div className="mb-2">Andrei Alvarico</div>
             <div className="mb-2">Claire Cruz</div>
