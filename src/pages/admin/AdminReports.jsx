f82eced22af28078a2f162c566960d238f425caa
jamessuuu <jameslorenzsantos@gmail.com>
2024-08-20 18:12:16 +0800

    Update AdminReports.jsx

--- src/pages/admin/AdminReports.jsx
+++ src/pages/admin/AdminReports.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function AdminReports() {
   return (
-    <div>AdminReports</div>
+    <div className='text-black font-light mx-4 md:mx-8 lg:mx-16 mt-8 mb-12'>
+      
+    </div>
   )
 }
 
